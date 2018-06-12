39dbe31727951c72b22b28f7645baabc91dc4104
Gareth Lowrie <gareth.lowrie@communicatorcorp.com>
2018-06-12 14:13:50 +0100

    removed rss

--- src/functions/rss.js
+++
@@ -1,8 +0,0 @@
-exports.handler = function(event, context, callback) {
-    let error = null;
-    let response = {
-        statusCode: 200,
-        body: "Hello, World"
-    };
-    callback(error, response);
-}
