4e5b39368ba09489081a8eadb839797f9c4dba7d
Gareth Lowrie <gareth.lowrie@communicatorcorp.com>
2018-09-27 11:43:30 +0100

    Updated resume

--- src/content/index.js
+++ src/content/index.js
@@ -24,9 +24,15 @@ export const EmploymentSection = ({ windowWidth }) => {
                 role="Front-end Developer"
                 startDate="March 2018"
                 endDate="Present"
-                tags={['Agile', 'SCRUM', 'HTML5', 'CSS3', 'JavaScript', 'React', 'git', 'Jira', 'Bitbucket']}
+                tags={['Agile', 'SCRUM', 'HTML5', 'CSS3', 'JavaScript', 'React', 'TypeScript', 'Redux', 'Redux-saga', 'Recompose', 'lodash', 'git', 'Jira', 'Bitbucket', 'GitLab', 'Octopus', 'TeamCity']}
             >
-                <p>After training alongside the Product Development Team I secured a position as Front-end Developer and became part of a team of three who are responsible for maintain and developing the next generation UI of the Communicator Suite.</p>
+                <div>
+                  <p>After training alongside the Product Development Team, I secured the position of Front-end Developer specialising in React.js and became part of a team of three who are responsible for maintaining and developing the next generation of our companies SaaS offering. I am one of two front-end developers working on our innovative new product due to go to market very shortly.</p>
+                  <br />
+                  <p>As a team we follow scrum and agile methodologies working to a two-week sprint cycle. To achieve this, we partake in daily stand-ups, and bi-weekly sprint review, planning and retrospective sessions.</p>
+                  <br />
+                  <p>We use Atlassian's offerings to track our progress combined with TeamCity and Octopus to push out updates into our production environments.</p>
+                </div>
             </Job>
             <Job
                 company="Communicator"
@@ -35,7 +41,17 @@ export const EmploymentSection = ({ windowWidth }) => {
                 endDate="March 2018"
                 tags={['Customer Service', 'SQL', 'Jira Service Desk Administration', 'Bug Fixing', 'Confluence', 'User Guide Creation', 'Blog Creator', '5 Star Satisfaction Rating']}
             >
-            <p>After graduating I joined Communicator as a Product Operations Analyst where I provided exceptional technical support both internally and to customers. This opportunity acted as a stepping stone on my career path as this allowed me to train on the job to fulfil my ambition to become a Front-end Developer.</p>
+              <div>
+                <p>As a Product Operations Analyst I provided exceptional technical support both internally and to customers.</p>
+                <br />
+                <p>Communicator is a feature-rich highly innovative bespoke solution, that’s been specifically designed to be versatile, so that each implementation is bespoke to the individual customer, therefore the technical queries that are received are not repetitive, but are as diverse as their customer base.</p>
+                <br />
+                <p>Depending upon the nature of the customer’s query, they may need support via email or telephone, require a demonstration, coaching, conference call or prefer to discuss their query face to face during a scheduled visit to their office or Communicator's.</p>
+                <br />
+                <p>A few months in I started working in the Product Development team part-time which gradually progressed into successfully securing a full-time position as a Junior Front-end Developer.</p>
+                <br />
+                <p>This opportunity has allowed me to develop my knowledge of the latest front-end technologies including but not limited to JavaScript, ES6 and React.js whilst exposing me to working in an agile and SCRUM environment.</p>
+              </div>
             </Job>
         </div>
     )
@@ -55,7 +71,7 @@ export const ProfessionalProfileSection = ({ windowWidth }) => {
   return (
       <div className={styles.contentSection}>
         <h2>PROFESSIONAL PROFILE</h2>
-        <p className={styles.tagline}>Hey. I'm Gareth, an enthusiastic UX developer. I turn beautiful designs into stateful applications using HTML, CSS, JavaScript, ES6, React and more. I'm a high flying guy with academic excellence and a real passion for the latest technologies. Want to know more? Take a scroll!</p>
+        <p className={styles.tagline}>I’m Gareth, an enthusiastic Front-end Developer. I turn beautiful designs into applications using HTML, CSS, JavaScript, ES6+ React and more. I’m a high-flying guy with academic excellence and a real passion for the latest technologies. Although specialising in Front-end development, I’m actively looking into building full-stack apps with React and Node JS in my free time.<br />Want to know more? Take a scroll!</p>
         {windowWidth < mobileBreakpoint860 &&
           <span><a href="https://github.com/garethlowrie/portfolio/raw/master/src/assets/Gareth%20Lowrie%20Resume.pdf" target="_blank" className={styles.mobileButton}>Download CV</a>
           <a href="https://linkedin.com/in/garethlowrie" target="_blank" className={styles.mobileButton}>Linked In</a></span>}
@@ -72,13 +88,13 @@ export const SkillsSection = ({ windowWidth }) => {
           <Flex.Item basis={basis} grow={0}>
             <div>
               <p>JavaScript and Libraries</p>
-              <Tags items={['ES6', 'React', 'Recompose', 'lodash', 'Formik']} />
+              <Tags items={['ES6+', 'React', 'Redux', 'Redux-saga', 'TypeScript', 'Recompose', 'lodash', 'Formik']} />
             </div>
           </Flex.Item>
           <Flex.Item basis={basis} grow={0}>
             <div>
               <p>Markup and Styling</p>
-              <Tags items={['HTML5', 'CSS3', 'LESS', 'SVG', 'JSON', 'XML', 'Bootstap', 'FontAwesome']} />
+              <Tags items={['HTML5', 'CSS3', 'LESS', 'SVG', 'JSON', 'XML', 'Bootstrap', 'FontAwesome']} />
             </div>
           </Flex.Item>
         </Flex>
@@ -86,7 +102,7 @@ export const SkillsSection = ({ windowWidth }) => {
           <Flex.Item basis={basis}>
             <div>
               <p>Development and Build Tools</p>
-              <Tags items={['git', 'npm', 'Webpack', 'Gatsby']} />
+              <Tags items={['git', 'npm', 'Webpack', 'Gatsby', 'GitLab', 'Bitbucket', 'GitHub', 'Octopus', 'TeamCity']} />
             </div>
           </Flex.Item>
           <Flex.Item basis={basis}>
